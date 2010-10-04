01c75c0a6c8f73e5fe658f4b6e0b137d7371a732
Matthias Treydte <waldheinz@gmail.com>
2010-10-04 15:56:12 +0200

    Added solution for problem 3.

--- problem-003.hs
+++ problem-003.hs
@@ -1,3 +1,18 @@
 
+import Debug.Trace
+
+-- | Turner's sieve
+primes :: [Integer]
+primes = sieve [2..] where
+   sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]  
+
+largestPf :: Integer -> Integer
+largestPf x = f primes x 0 where
+   f (p:ps) x lrg
+      | p == x = p
+      | x `mod` p == 0 = f ps (x `div` p) p
+      | p >= x = lrg
+      | otherwise = f ps x lrg
+
 main :: IO()
-main = putStrLn "hello"
+main = putStrLn $ show $ largestPf 600851475143
