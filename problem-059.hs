d7cea36ce71c5dc245e559e1b66b62267d1f093b
Matthias Treydte <waldheinz@gmail.com>
2010-10-06 12:09:47 +0200

    Finished problem 59.

--- problem-059.hs
+++ problem-059.hs
@@ -1,4 +1,5 @@
 
+import Data.Bits
 import Bits
 import Int
 import Char
@@ -17,9 +18,27 @@ passes = map (cycle . (map (fromIntegral . ord))) cands
 --decode :: [Int32] -> [Int32] -> String
 decode m p = map chr $ map fromIntegral $ zipWith xor m p
 
+fitness :: String -> Int
+fitness [] = 0
+fitness (c:cs)
+   | c == ' ' = 1 + fitness cs
+   | otherwise = fitness cs
+
+comp :: (String, Int) -> (String, Int) -> (String, Int)
+comp a@(ma, fa) b@(mb, fb)
+   | fa > fb = a
+   | otherwise = b
+
+charSum :: String -> Int
+charSum [] = 0
+charSum (c:cs) = (ord c) + charSum cs
+
 crack :: String -> String
-crack i = show ints where
-   ints =  map read $ splitBy (==',') i :: [Int32]
+crack i = show $ charSum $ fst best where
+   best = foldl comp ("", 0) fits
+   fits = zip decs (map fitness decs)
+   decs = map (decode enc) passes
+   enc =  map read $ splitBy (==',') i :: [Int32]
    
 main = do
    c <- readFile "problem-059.txt"
