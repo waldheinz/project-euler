f2f644c7b1616b3b3a591bce8ed1be946ea3f3ae
Matthias Treydte <waldheinz@gmail.com>
2010-10-06 13:19:13 +0200

    Added solution for problem 18.

---
+++ problem-018.hs
@@ -0,0 +1,15 @@
+
+readTri :: String -> [[Int]]
+readTri s = map (\l -> map read $ words l) $ lines s
+
+--paths :: Int -> Int -> [[Int]] -> [Int]
+--paths p c [] = c
+--paths p c (l:ls) = (l !! p) + 
+
+p18 = foldr1 g where
+   f x y z = x + max y z
+   g xs ys = zipWith3 f xs ys $ tail ys
+
+main = do
+   input <- readFile "problem-018.txt"
+   print $ head $ p18 $ readTri input
