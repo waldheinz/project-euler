c37404c567734963d2f16f766be5f16557cd9445
Matthias Treydte <waldheinz@gmail.com>
2011-11-30 11:26:46 +0100

    Finished solution for problem 23.

--- problem-023.hs
+++ problem-023.hs
@@ -1,8 +1,16 @@
 
-divs x = filter (\d -> x `mod` d == 0) [1..(x `div` 2)]
-abun x = (> x) . sum $ divs x
-abunds = filter abun [1..]
-abunp x = [(a, b) | a <- takeWhile (< x `div` 2) abunds, let b = x-a, abun b]
-sabun = not . null . abunp
+import Debug.Trace
 
-main = print $ sum $ filter (not . sabun) [1..28123]
+divs :: Int -> [Int]
+divs x = filter (\d -> x `rem` d == 0) [1..(x `div` 2)]
+
+abun :: Int -> Bool
+abun x = (> x) . sum $ divs x
+
+abunds :: [Int]
+abunds = takeWhile (< 15000) $ filter abun [1..]
+
+abunp :: Int -> [(Int, Int)]
+abunp x = [(a, b) | a <- takeWhile (<= x `div` 2) abunds, let b = x-a, abun b]
+
+main = print $ sum $ filter (\x -> trace (show x) $ (null . abunp) x) [1..28123]
