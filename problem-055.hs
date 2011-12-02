0a777673bb58de21f237d46283ed5bf350ae683f
Matthias Treydte <waldheinz@gmail.com>
2011-12-02 13:01:44 +0100

    Added solution for problem 55.

---
+++ problem-055.hs
@@ -0,0 +1,14 @@
+
+-- would be sufficient to check half the digits, but so what...
+palin :: Integer -> Bool
+palin n = let s = show n in all (uncurry (==)) $ zip s $ reverse s
+
+step :: Integer -> Integer
+step n = n + (read.reverse.show) n
+
+nlychrel :: Integer -> Bool
+nlychrel n = any palin $ take 50 $ tail $ iterate step n
+
+main :: IO ()
+main = print $ fromIntegral n - (length $ filter nlychrel [1..n]) where
+   n = 10000
