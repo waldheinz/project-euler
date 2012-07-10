2638298c514287d99122be98f59abea950b5b592
Matthias Treydte <waldheinz@gmail.com>
2012-07-10 12:54:46 +0200

    Finished problem 79.

--- problem-079.hs
+++ problem-079.hs
@@ -1,36 +1,22 @@
 
-import Data.List (tails, sortBy)
-import Data.Function (on)
-import Debug.Trace
+import Data.Char (digitToInt)
+import Data.List (tails, nub, intersect)
+import Data.Graph (buildG, topSort)
 
-valid :: [String] -> String -> Bool
-valid ks s = all (valid' s) ks where
-   valid' :: String -> String -> Bool
-   valid' [] cs = null cs
-   valid' _ [] = True
-   valid' s (c:cs)
-      | null rest = False
-      | otherwise = valid' (tail rest) cs
-      where
-         rest = dropWhile (/= c) s
-
-shrink :: Int -> [String] -> String -> [String]
-shrink _ _ [] = []
-shrink sl ks all@(x:xs)
-   | length all > sl = []
-   | not $ valid ks all = []
-   | valid ks xs = xs : rest
-   | otherwise = rest
-   where
-      rest = concatMap (shrink sl' ks) $ map (x:) $ tail $ tails xs
-      sl' = if valid ks xs then length xs else sl
 
 main :: IO ()
 main = do
-   keys <- fmap lines $ readFile "keylog.txt"
-   let
-      start = concat keys
-      x = shrink (length start) keys start
-   print $ x
---    print $ all (valid keys) x
-   print $ head $ sortBy (compare `on` length) x
+	keys <- readFile "data/keylog.txt"
+	let
+	   edges = concatMap (\(a:b:c:[]) ->
+			[(digitToInt a, digitToInt b), (digitToInt b, digitToInt c)]) $ lines keys
+			
+	   g = buildG (0, 9) edges
+	   used = intersect ['0'..'9'] keys
+	
+	putStrLn $ "used chars : " ++ (show used)
+	putStrLn $ "solution : " ++ (show $ intersect (concatMap show $ topSort g) used)
+	putStrLn $ show edges
+	putStrLn $ show $ nub edges
+	putStrLn $ show $ concatMap show $ topSort g
+   
